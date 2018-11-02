f36013fbb4da1e57a393cfa93cfb1889a73c3e6c
pinnouse <cupofpho@gmail.com>
2018-11-02 17:41:36 -0400

    CCC15S3 - Lower bound

--- CCC/CCC15S3.cpp
+++ CCC/CCC15S3.cpp
@@ -12,19 +12,19 @@ int main() {
 
     //Initialize binary tree of gates
     for(int i = 1; i <= G; i++)
-      gates.insert(i);
+      gates.insert(-i);
 
     //Planes loop
     for(int i = 0, p; i < P; i++) {
       scanf("%d", &p);
       //Reached the lower limit of gates
-      if (gates.upper_bound(p) == gates.begin()) {
+      if (gates.lower_bound(-p) == gates.end()) {
         printf("%d\n", i);
         return 0;
       }
 
       //Erase gates larger than p
-      gates.erase(gates.upper_bound(p));
+      gates.erase(gates.lower_bound(-p));
     }
 
     //Reach the end - all planes have found a gate
