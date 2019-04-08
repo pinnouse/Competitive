615453062c9a45f449678bf5ae2efe9c1257fa42
pinnouse <cupofpho@gmail.com>
2019-04-07 20:41:34 -0400

    DMOJdumb

--- CCC/CCC19S1.java
+++ CCC/CCC19S1.java
@@ -5,27 +5,27 @@ public class S1 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] flips = br.readLine().split("");
+		String flips = br.readLine();
 		boolean h = false, v = false;
-		for (int i = 0; i < flips.length; i++) {
-			if (flips[i].equalsIgnoreCase("h")) h = !h;
-			else if (flips[i].equalsIgnoreCase("v")) v = !v;
-		}
+
+		h = flips.chars().filter(ch -> ch == 'H').count() % 2 == 1;
+		v = (h && flips.length() % 2 == 0) || (!h && flips.length() % 2 == 1);
+
 		if (!v && !h) {
 			System.out.println("1 2");
 			System.out.println("3 4");
 		}
-		
+
 		else if (v && !h) {
 			System.out.println("2 1");
 			System.out.println("4 3");
 		}
-		
+
 		else if (v && h) {
 			System.out.println("4 3");
 			System.out.println("2 1");
 		}
-		
+
 		else if (!v && h) {
 			System.out.println("3 4");
 			System.out.println("1 2");
