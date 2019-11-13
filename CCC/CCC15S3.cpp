d1cddc1a3d7c03ca51cbee7d9b10e456fb3db88e
Nick <cupofpho@gmail.com>
2019-11-13 01:20:30 -0500

    Better understanding of the problem
    Fixed up documentation for self.

--- CCC/CCC15S3.cpp
+++ CCC/CCC15S3.cpp
@@ -7,23 +7,24 @@ int G, P;
 int main() {
     scanf("%d%d", &G, &P);
     
-    //Binary tree
+    //Initialize a set of ints to store our gates
     set<int> gates;
 
-    //Initialize binary tree of gates
+    //Initialize set with gate values
     for(int i = 1; i <= G; i++)
       gates.insert(-i);
 
     //Planes loop
     for(int i = 0, p; i < P; i++) {
       scanf("%d", &p);
-      //Reached the lower limit of gates
+      //The plane cannot find a gate or is the last gate
       if (gates.lower_bound(-p) == gates.end()) {
         printf("%d\n", i);
         return 0;
       }
 
-      //Erase gates larger than p
+      //Erase gates larger than p from our gates so no
+      //planes can get to those gates
       gates.erase(gates.lower_bound(-p));
     }
 
